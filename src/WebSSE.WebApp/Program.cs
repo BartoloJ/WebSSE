f2cc872166a5973d1c3a43298d3bb569e899ba79
Jonathan Bartolo <jonathan.bartolo@stmartin.edu>
2024-11-26 13:50:09 -0800

    Middleware

--- src/WebSSE.WebApp/Program.cs
+++ src/WebSSE.WebApp/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapGet("/sse", async (HttpContext context) =>
+{
+    context.Response.Headers[HeaderNames.ContentType] = "text/event-stream";
+    for (var index = 0; index < 10; index++)
+    {
+        await context.Response.WriteAsync($"data: Message {index}\n\n");
+        await context.Response.Body.FlushAsync();
+        await Task.Delay(1000);
+    }
+});
+
 
 app.Run();
